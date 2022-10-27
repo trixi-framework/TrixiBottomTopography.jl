3fb96f7bbe82ea6f02c9cad3220bfbba064534b3
maxbertrand1996 <maxbertrand1996@googlemail.com>
2022-10-27 09:36:26 +0200

    Test Documentation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# TrixiBottomTopography
+# TrixiBottomTopography.jl
+
+[![Docs-stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://maxbertrand1996.github.io/TrixiBottomTopography.jl/stable)
+[![Docs-dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://maxbertrand1996.github.io/TrixiBottomTopography.jl/dev)
+
 Approximating real life data for bottom topographies
--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,32 @@
 using Documenter
+import Pkg
 using TrixiBottomTopography
 
+# Define module-wide setups such that the respective modules are available in doctests
+DocMeta.setdocmeta!(TrixiBottomTopography, :DocTestSetup, :(using TrixiBottomTopography); recursive=true)
+
+# Make documentation
 makedocs(
     sitename = "TrixiBottomTopography.jl",
-    format = Documenter.HTML(),
+    format = Documenter.HTML(
+        # Set canonical URL to GitHub pages URL
+        canonical = "https://maxbertrand1996.github.io/TrixiBottomTopography.jl/dev"
+    ),
     modules = [TrixiBottomTopography],
     pages = [
         "Home" => "index.md",
-        "API" => "reference.md"
-    ]
+        "Reference" => "reference.md",
+        "Licence" => "licence.md"
+    ],
+    # strict = true # to make the GitHub action fail when doctests fail
+    strict = Documenter.except(:cross_references)
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "https://github.com/maxbertrand1996/TrixiBottomTopography.jl",
+    devbranch = "main",
+    push_preview = true
+)
