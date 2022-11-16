b785176fe7cea5bdf334bb71adcc03c8a75ab6dd
maxbertrand1996 <maxbertrand1996@googlemail.com>
2022-11-16 10:27:11 +0100

    Update test Bilinear

--- test/test_bilinear_b_spline.jl
+++ test/test_bilinear_b_spline.jl
@@ -9,4 +9,4 @@ spline_struct = BilinearBSpline(data)
 spline_func(x, y) = spline_interpolation(spline_struct, x, y)
 
 # Test function at arbitrary point
-@test spline_func(357555, 5646555) == 39.260000000000005
+@test 39.26 <= spline_func(357555, 5646555) <= 39.26000000000001
