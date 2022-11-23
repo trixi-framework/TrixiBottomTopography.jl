4d9f68a45822aa3d7f56bbee4fea97babc6d7ac4
Andrew Winters <andrew.ross.winters@liu.se>
2022-11-23 19:55:44 +0100

    suggestions to the 1D code

--- src/1D/spline_utils_1D.jl
+++ src/1D/spline_utils_1D.jl
@@ -1,23 +1,23 @@
 
-#################################################
-### Helpfunctions for 1D spline interpolation ###
-#################################################
+####################################################
+### Helper functions for 1D spline interpolation ###
+####################################################
 
 # Sorting the inputs so that the x values are ascending
 """
     sort_data(x::Vector,y::Vector)
 
-Sorts the input vectors `x` and `y` so that `x` is in ascending order and the `y` values still
-correspond to the `x` values.
+Sorts the input vectors `x` and `y` so that `x` is in ascending order and the `y` values
+accordingly to still correspond to the `x` values.
 """
 function sort_data(x::Vector,y::Vector)
-      
+
   original_data = hcat(x,y)
   sorted_data = original_data[sortperm(original_data[:,1]), :]
-  
+
   x_sorted = sorted_data[:,1]
   y_sorted = sorted_data[:,2]
-  
+
   return x_sorted,y_sorted
 end
 
@@ -26,19 +26,19 @@ end
     spline_smoothing(lambda::Number, h::Number, y::Vector)
 
 The inputs to this function are:
-- `lambda`: a smoothing factor which specifies the degree of the smoothing that should take place
-- `h`: the step size of a patch (A patch is the area between two consecutive `x` values)
-- `y`: the `y` values to be smoothed
+- `lambda`: Smoothing factor which specifies the degree of the smoothing that should take place
+- `h`: Step size of a patch (A patch is the area between two consecutive `x` values)
+- `y`: Data values to be smoothed
 
-The goal is to find a new interpolation values ``\hat{y}`` for ``y``, so that for given ``\lambda``, 
+The goal is to find a new interpolation values ``\hat{y}`` for ``y``, so that for given ``\lambda``,
 the following equation is minimized:
 ```math
 \begin{aligned}
-		\text{PSS} = \sum_{i = 1}^{n} \left( y_i - \underbrace{S(t_i)}_{=\hat{y}_i} \right)^2 
+		\text{PSS} = \sum_{i = 1}^{n} \left( y_i - \underbrace{S(t_i)}_{=\hat{y}_i} \right)^2
     + \lambda \int_{x_1}^{x_n} (S''(t))^2 dt,
 	\end{aligned}
 ```
-where ``S(t)`` is a cubic spline function. 
+where ``S(t)`` is a cubic spline function.
 ``\hat{y}`` is determined as follows:
 ```math
 \begin{aligned}
@@ -68,7 +68,7 @@ W = \begin{pmatrix}
 ```
 
 - Germán Rodríguez (2001),
-[Smoothing and non-parametric regression](https://data.princeton.edu/eco572/smoothing.pdf)
+  [Smoothing and non-parametric regression](https://data.princeton.edu/eco572/smoothing.pdf)
 """
 function spline_smoothing(lambda::Number, h::Number, y::Vector)
 
@@ -79,7 +79,7 @@ function spline_smoothing(lambda::Number, h::Number, y::Vector)
   Delta_ii   =  1 ./ h_vec
   Delta_iip1 = -2 ./ h_vec
   Delta_iip2 =  1 ./ h_vec
-  
+
   Delta             =  zeros(n-2, n)
   Delta[:, 1:(n-2)] =  diagm(Delta_ii)
   Delta[:, 2:(n-1)] += diagm(Delta_iip1)
