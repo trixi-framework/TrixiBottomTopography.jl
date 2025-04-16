e80d303094a4b8713dc0efc3bfadc831dbd31a6e
Andrew Winters <andrew.ross.winters@liu.se>
2025-04-16 12:22:14 +0200

    Adjust docs build (#47)
    * add zenodo info
    
    * add stable docs badge
    
    * import README to create the landing page in the docs
    
    * revert to released version
    
    * set compat bound to Documenter v1.3 and adjust files accordingly
    
    * simplify file digestion and creation
    
    * adjustments to (hopefully) fix build issues
    
    * adjust docs build based on comments
    
    * fix url for the zenodo badge
    
    * put back in the EditURL

--- docs/make.jl
+++ docs/make.jl
@@ -2,10 +2,13 @@ using Documenter
 import Pkg
 using TrixiBottomTopography
 
-# Copy list of authors to not need to synchronize it manually
+# Copy list of authors to not need to synchronize it manually.
+# Since the authors header exists twice we create a unique identifier for the docs section.
 authors_text = read(joinpath(dirname(@__DIR__), "AUTHORS.md"), String)
 authors_text = replace(authors_text,
                        "in the [LICENSE.md](LICENSE.md) file" => "under [License](@ref)")
+authors_text = replace(authors_text,
+                       "# Authors" => "# [Authors](@id trixi_bt_authors)")
 write(joinpath(@__DIR__, "src", "authors.md"), authors_text)
 
 # Define module-wide setups such that the respective modules are available in doctests
@@ -27,11 +30,12 @@ open(joinpath(@__DIR__, "src", "code_of_conduct.md"), "w") do io
     println(io, "# [Code of Conduct](@id code-of-conduct)")
     println(io, "")
     for line in eachline(joinpath(dirname(@__DIR__), "CODE_OF_CONDUCT.md"))
-        line = replace(line, "[AUTHORS.md](AUTHORS.md)" => "[Authors](@ref)")
+        line = replace(line, "[AUTHORS.md](AUTHORS.md)" => "[Authors](@ref trixi_bt_authors)")
         println(io, "> ", line)
     end
 end
 
+# Copy contributing information to not need to synchronize it manually
 open(joinpath(@__DIR__, "src", "contributing.md"), "w") do io
     # Point to source license file
     println(io,
@@ -43,35 +47,51 @@ open(joinpath(@__DIR__, "src", "contributing.md"), "w") do io
     # Write the modified contents
     for line in eachline(joinpath(dirname(@__DIR__), "CONTRIBUTING.md"))
         line = replace(line, "[LICENSE.md](LICENSE.md)" => "[License](@ref)")
-        line = replace(line, "[AUTHORS.md](AUTHORS.md)" => "[Authors](@ref)")
+        line = replace(line, "[AUTHORS.md](AUTHORS.md)" => "[Authors](@ref trixi_bt_authors)")
         println(io, line)
     end
 end
 
+# Copy contents form README to the starting page to not need to synchronize it manually
+readme_text = read(joinpath(dirname(@__DIR__), "README.md"), String)
+readme_text = replace(readme_text,
+                      "[LICENSE.md](LICENSE.md)" => "[License](@ref)")
+readme_text = replace(readme_text,
+                      "[AUTHORS.md](AUTHORS.md)" => "[Authors](@ref trixi_bt_authors)")
+readme_text = replace(readme_text,
+                      "<p" => "```@raw html\n<p")
+readme_text = replace(readme_text,
+                      "p>" => "p>\n```")
+readme_text = replace(readme_text,
+                      r"\[comment\].*\n" => "")    # remove comments
+write(joinpath(@__DIR__, "src", "index.md"), readme_text)
+
 # Make documentation
-makedocs(
-    sitename = "TrixiBottomTopography.jl",
-    format = Documenter.HTML(
-        # Set canonical URL to GitHub pages URL
-        canonical = "https://trixi-framework.github.io/TrixiBottomTopography.jl/dev"
-    ),
-    modules = [TrixiBottomTopography],
-    pages = [
-        "Home" => "index.md",
-        "Overview" => [
-            "Data conversion" => "conversion.md",
-            "B-spline structure" => "structure.md",
-            "B-spline function" => "function.md",
-        ],
-        "Trixi.jl examples" => "Trixi.md",
-        "Reference" => "reference.md",
-        "Authors" => "authors.md",
-        "Contributing" => "contributing.md",
-        "Code of Conduct" => "code_of_conduct.md",
-        "Licence" => "licence.md"
-    ],
-    strict = true # to make the GitHub action fail when doctests fail
-)
+makedocs(;
+         modules = [TrixiBottomTopography],
+         authors = "Andrew R. Winters <andrew.ross.winters@liu.se>, Michael Schlottke-Lakemper <michael@sloede.com>",
+         sitename = "TrixiBottomTopography.jl",
+         format = Documenter.HTML(;
+                                  # Disable pretty URLs during manual testing
+                                  prettyurls = get(ENV, "CI", "false") == "true",
+                                  # Set canonical URL to GitHub pages URL
+                                  canonical = "https://trixi-framework.github.io/TrixiBottomTopography.jl/stable",
+                                  edit_link = "main",
+                                  size_threshold_ignore = ["index.md"],),
+         # Explicitly specify documentation structure
+         pages = [
+         "Home" => "index.md",
+         "Overview" => [
+             "Data conversion" => "conversion.md",
+             "B-spline structure" => "structure.md",
+             "B-spline function" => "function.md",
+         ],
+         "Trixi.jl examples" => "trixi_jl_examples.md",
+         "Authors" => "authors.md",
+         "Contributing" => "contributing.md",
+         "Code of Conduct" => "code_of_conduct.md",
+         "License" => "license.md",
+         "Reference" => "reference.md"])
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
