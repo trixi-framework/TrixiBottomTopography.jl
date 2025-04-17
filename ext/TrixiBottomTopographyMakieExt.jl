00786739d7e3a2b9eb5a7bcfa7320d36eb3ef419
Andrew Winters <andrew.ross.winters@liu.se>
2025-04-17 21:46:22 +0200

    add docstrings for the new plotting routines

--- ext/TrixiBottomTopographyMakieExt.jl
+++ ext/TrixiBottomTopographyMakieExt.jl
@@ -10,9 +10,13 @@ using TrixiBottomTopography
 # Import functions such that they can be extended with new methods
 import TrixiBottomTopography: evaluate_bicubicspline_interpolant, plot_topography, plot_topography_with_interpolation_knots
 
+"""
+    evaluate_bicubicspline_interpolant(spline, x, y)
 
-# Helper function to sample the bicubic spline function `spline`
-# at the nodes `x` and `y` and return the values in `z`
+Helper function to sample the bicubic spline function `spline`
+at the interpolation nodes `x` (with size `n`) and `y` (with size `m`)
+and return the values in an array `z` of size `m` by `n`.
+"""
 function evaluate_bicubicspline_interpolant(spline, x, y)
 
   # Get dimensions for solution matrix
@@ -31,17 +35,37 @@ function evaluate_bicubicspline_interpolant(spline, x, y)
   return z
 end
 
-# We set the Makie default colormap to `:greenbrownterrain`.
-# Other colormaps that may be of use are `:darkterrain`, `:sandyterrain` or `:gist_earth`
-# Notes, Plots.jl default is `:inferno`.
+# Set the Makie default colormap to `:greenbrownterrain`.
 default_Makie_colormap() = :greenbrownterrain
 
-# Generic plot function for 1D bottom topography data
+"""
+    plot_topography_with_interpolation_knots(x, y;
+                                             xlabel = "", ylabel= "",
+                                             color, legend_position = :rt)
+
+Plot function for 1D bottom topography data.
+The interpolated values are provided in the vectors `x` and `y`.
+Axis labels can be prescribed with `xlabel`, `ylabel`, and `zlabel`.
+The default `color` for the interpolated bottom topography is light blue.
+The `legend_position` controls the placement of the legend.
+"""
 function plot_topography(x, y; xlabel = "", ylabel= "", color = Makie.wong_colors(2)[5])
   Makie.lines(x, y, axis=(xlabel = xlabel, ylabel = ylabel), color = color)
 end
 
-# Plot function for 1D bottom topography data together with the interpolation knots
+"""
+    plot_topography_with_interpolation_knots(x, y, x_knots, y_knots;
+                                             xlabel = "", ylabel= "",
+                                             color, legend_position = :rt)
+
+Plot function for 1D bottom topography data together with the interpolation knots.
+The interpolated values are provided in the vectors `x` and `y`.
+The interpolation knots are provided in a similar fashion in vectors `x_knots` and
+`y_knots`.
+Axis labels can be prescribed with `xlabel`, `ylabel`, and `zlabel`.
+The default `color` for the interpolated bottom topography is light blue.
+The `legend_position` controls the placement of the legend.
+"""
 function plot_topography_with_interpolation_knots(x, y, x_knots, y_knots;
                                                   xlabel = "", ylabel= "",
                                                   color = Makie.wong_colors(2)[5],
@@ -60,21 +84,62 @@ function plot_topography_with_interpolation_knots(x, y, x_knots, y_knots;
   fig
 end
 
+"""
+    plot_topography(x, y, z;
+                    xlabel = "", ylabel= "", zlabel = "",
+                    colormap,
+                    azimuth_angle = 1.275*pi,
+                    elevation_angle = pi/8)
+
+Plot function for 2D bottom topography data.
+The interpolated values are provided in the vectors `x`, `y` and an array `z`
+where the shape of array `z` must be size(y) by size(x).
+Axis labels can be prescribed with `xlabel`, `ylabel`, and `zlabel`.
+The default `colormap` is `:greenbrownterrain`.
+Other possible colormaps for bottom topographies are `:darkterrain`, `:sandyterrain`,
+or `:gist_earth`.
+The left / right camera angle is controlled by `azimuth_angle` whereas the up / down
+camera angle is controlled by `elevation_angle`. Both angle arguments must be given in radians.
+"""
 # Generic plot function for 2D bottom topography data
 function plot_topography(x, y, z; xlabel = "", ylabel= "", zlabel = "",
-                         colormap = default_Makie_colormap())
+                         colormap = default_Makie_colormap(),
+                         azimuth_angle = 1.275*pi, elevation_angle = pi/8)
   Makie.surface(x, y, z, axis=(type = Makie.Axis3,
-                xlabel = xlabel, ylabel = ylabel, zlabel = zlabel),
+                xlabel = xlabel, ylabel = ylabel, zlabel = zlabel,
+                azimuth = azimuth_angle, elevation = elevation_angle),
                 colormap = colormap)
 end
 
-# Plot function for 2D bottom topography data together with the interpolation knots
+"""
+    plot_topography_with_interpolation_knots(x, y, z, x_knots, y_knots, z_knots;
+                                             xlabel = "", ylabel= "", zlabel = "",
+                                             colormap, legend_position = :rt,
+                                             azimuth_angle = 1.275*pi,
+                                             elevation_angle = pi/8)
+
+Plot function for 2D bottom topography data together with the interpolation knots.
+The interpolated values are provided in the vectors `x`, `y` and an array `z`
+where the shape of array `z` must have size(y) by size(x).
+The interpolation knots are provided in a similar fashion in vectors `x_knots`,
+`y_knots` and an array `z_knots` where the shape of `z_knots` must have size(y_knots) by size(x_knots).
+Axis labels can be prescribed with `xlabel`, `ylabel`, and `zlabel`.
+The default `colormap` is `:greenbrownterrain`.
+Other possible colormaps for bottom topographies are `:darkterrain`, `:sandyterrain`,
+or `:gist_earth`.
+The `legend_position` controls the placement of the legend.
+The left / right camera angle is controlled by `azimuth_angle` whereas the up / down
+camera angle is controlled by `elevation_angle`. Both angle arguments must be given in radians.
+"""
 function plot_topography_with_interpolation_knots(x, y, z, x_knots, y_knots, z_knots;
                                                   xlabel = "", ylabel= "", zlabel = "",
                                                   colormap = default_Makie_colormap(),
-                                                  legend_position = :rt)
+                                                  legend_position = :rt,
+                                                  azimuth_angle = 1.275*pi,
+                                                  elevation_angle = pi/8)
   fig = Makie.Figure()
-  ax = Makie.Axis3(fig[1, 1], xlabel = xlabel, ylabel = ylabel, zlabel = zlabel)
+  ax = Makie.Axis3(fig[1, 1], xlabel = xlabel, ylabel = ylabel, zlabel = zlabel,
+                   azimuth = azimuth_angle, elevation = elevation_angle)
 
   # Approximate topography with a line
   Makie.surface!(ax, x, y, z, colormap = colormap, label = "Bottom topography")
