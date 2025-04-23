6321388de80badc4ac8819b3411dce4ac6422722
Andrew Winters <andrew.ross.winters@liu.se>
2025-04-23 19:46:27 +0200

    remove trailing whitespace

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,17 +4,17 @@ using Test
 
 # Start testing
 @testset "TrixiBottomTopography.jl" begin
-    
+
   # Test data conversion
   @testset "Conversion" begin
     include("test_convert.jl")
   end
-  
+
   # Linear B-splines
   @testset "Linear B-spline interpolation" begin
     include("test_linear_b_spline.jl")
   end
-  
+
   # Cubic B-splines
   @testset "Cubic B-spline interpolation" begin
     include("test_cubic_b_spline.jl")
