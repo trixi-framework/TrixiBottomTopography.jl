5a3e41f7ae0a5214733e7ed05c7d632316c05017
Andrew Winters <andrew.ross.winters@liu.se>
2025-04-24 21:28:11 +0200

    fix docstring for default_example

--- src/auxiliary/default_example.jl
+++ src/auxiliary/default_example.jl
@@ -1,9 +1,9 @@
 """
     default_example()
 
-Function which calls the example file "rhine_data_bicubic-nak.jl" as a default example to
-check if including the package has worked creates a bicubic B-spline
-with the "not-a-knot" boundary condition.
+Function which calls the example file "rhine_data_bicubic-nak.jl" to
+check if including the package has worked. The example script creates
+a bicubic B-spline with the "not-a-knot" boundary condition.
 If a [Makie.jl](https://github.com/JuliaPlots/Makie.jl/) backend
 such as [GLMakie.jl](https://github.com/JuliaPlots/GLMakie.jl/)
 is included a plot will be generated as well.
