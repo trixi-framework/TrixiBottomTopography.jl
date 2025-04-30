92773d3b04c49405e1c82d1d68038a6cfdc4a494
vincmarks <vimarks@students.uni-mainz.de>
2025-04-30 19:05:06 +0200

    add: convert2CartData

--- examples/geo_data.jl
+++ examples/geo_data.jl
@@ -23,6 +23,9 @@ lon_max = 8.277027
 lat_min = 50.007888
 lat_max = 50.008306
 limits=[lon_min, lon_max, lat_min, lat_max]
+
+lon_mean = (lon_max+lon_min)/2
+lat_mean=(lat_min+lat_max)/2
 ############################
 #Loading the topography data
 #important: if you have a small area you might use hiher resolution: 
@@ -53,13 +56,33 @@ limits=[lon_min, lon_max, lat_min, lat_max]
 
 Topo = import_topo(limits; file="@earth_relief_01s", maxattempts=5) 
 
+
+#Topo = import_topo(lon = [-10, 45], lat=[25, 50], file="@earth_relief_20m")
+p=ProjectionPoint(Lon=lon_mean, Lat=lat_mean)
+Topo_Cart = convert2CartData(Topo,p)#Topo_Cart = convert2UTMzone(Topo,p) #convert2UTMzone(Topo,p),convert2CartData(Topo,p)
+write_paraview(Topo_Cart,"Rhein_highest");
+
 ############################
 #building a .vts file to first get an impression of the topography data in Paraview
 
-write_paraview(Topo, "topography_rhein") 
+
     
 #Uses `GMT` to download the topography of a certain region, specified with limits=[lon_min, lon_max, lat_min, lat_max].
 #Sometimes download fails because of the internet connection. We do `maxattempts` to download it.
 
 # using GeophysicalModelGenerator, GMT
 # Topo = import_topo([4,20,37,49], file="@earth_relief_01m")
+
+Topo = import_topo(lon = [-10, 45], lat=[25, 50], file="@earth_relief_20m")
+
+convert(UTMData, Topo)
+
+p=ProjectionPoint(Lon=17.3, Lat=37.5)
+
+convert2UTMzone(Topo,p)
+
+Topo_Cart = convert2CartData(Topo,p)
+
+Topo_Cart_orth  = CartData(xyz_grid(-2000:20:2000,-1000:20:1000,0))
+
+Topo_Cart_orth  = project_CartData(Topo_Cart_orth, Topo, p)
