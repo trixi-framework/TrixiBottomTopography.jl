d1727287a26a4106a05ddf093b1103bfc4217d75
vincmarks <vimarks@students.uni-mainz.de>
2025-05-13 10:39:13 +0200

    trying to get somewhere

--- examples/geo_data.jl
+++ examples/geo_data.jl
@@ -4,28 +4,69 @@
 # es gibt import topo, gmg laden und gmt, dann gibt es eine funktion import_topo, denn gebe ich die longitude und lattitude ein und dann läd man das runter
 # 
 import Pkg
-Pkg.add(["GeophysicalModelGenerator", "GMT", "Plots"])
+Pkg.add(["GeophysicalModelGenerator", "GMT", "Plots","CSV","DataFrames","TrixiBottomTopography", "Downloads"])
 using GMT
 using Plots
 using GeophysicalModelGenerator
-
+using CSV
+using DataFrames
+using TrixiBottomTopography
+using Downloads: download
 
 ##########################
-#some topography data from the Rhine close to the Theodor-Heuss-Brücke in Mainz
-#50.008306, 8.276694
-#50.008200, 8.277027
-#50.007888, 8.276223
-#50.008217, 8.275613
+# some topography data from the Rhine close to the Theodor-Heuss-Brücke in Mainz
+#50.010910 8.275880
+#50.009480 8.270871
+#50.006184 8.274388
+#50.007914 8.278718
+
+
+
 ###########################
 # Specify the limits of the topography data based on the coordinates
-lon_min = 8.276223
-lon_max = 8.277027
-lat_min = 50.007888
-lat_max = 50.008306
+lon_min = 8.270871
+lon_max = 8.278718
+lat_min = 50.006184 
+lat_max = 50.010910
 limits=[lon_min, lon_max, lat_min, lat_max]
 
 lon_mean = (lon_max+lon_min)/2
 lat_mean=(lat_min+lat_max)/2
+
+#rausfinden wie man die richtigen topographischen daten bekommt, anscheinend ist die gewählte form: 
+#the first column provides the corresponding ETRS89 East coordinates, the second column the 
+#ETRS89 North coordinates and the third column the DHHN2016 height.
+
+###
+#Lon=17.3, Lat=37.5
+#Topo = import_topo(limits, file="@earth_relief_20m")
+Topo = import_topo(lon = [8.270871, 8.278718], lat=[50.006184, 50.010910], file="@earth_relief_01m")
+p=ProjectionPoint(Lon=lon_mean, Lat=lat_mean)
+Topo_Cart = convert2CartData(Topo,p)
+
+Topo_Cart_orth  = CartData(xyz_grid(-0.5:0.1:0.5,-0.9:0.1:0.9,0))
+Topo_Cart_orth  = project_CartData(Topo_Cart_orth, Topo, p)
+
+df_x = DataFrame(Topo_Cart_orth.x.val[:,:,1], :auto)
+
+df_y = DataFrame(Topo_Cart_orth.y.val[:,:,1], :auto);
+
+df_z = DataFrame(Topo_Cart_orth.z.val[:,:,1], :auto);
+
+# Kombiniere die DataFrames für x, y und z in einen DataFrame
+df_xyz = DataFrame(x = vec(Topo_Cart_orth.x.val[:,:,1]), 
+                   y = vec(Topo_Cart_orth.y.val[:,:,1]), 
+                   z = vec(Topo_Cart_orth.z.val[:,:,1]))
+
+# Schreibe die Datei ohne Header und mit Leerzeichen als Delimiter
+open("test.xyz", "w") do file
+    for row in eachrow(df_xyz)
+        println(file, join(row, " "))
+    end
+end
+
+
+
 ############################
 #Loading the topography data
 #important: if you have a small area you might use hiher resolution: 
@@ -52,9 +93,14 @@ lat_mean=(lat_min+lat_max)/2
 # | "@earth\\_relief\\_30m"	|  30 arc min	 | ETOPO1 after Gaussian spherical filtering (55 km fullwidth) |
 # | "@earth\\_relief\\_60m"	|  60 arc min	 | ETOPO1 after Gaussian spherical filtering (111 km fullwidth)|
 
-# *Note*: this routine is only available once the GMT.jl package is loaded in the REPL
 
-Topo = import_topo(limits; file="@earth_relief_01s", maxattempts=5) 
+Topo = import_topo(lon = [-10, 45], lat=[25, 50], file="@earth_relief_20m")
+p=ProjectionPoint(Lon=17.3, Lat=37.5)
+convert2UTMzone(Topo,p)
+Topo_Cart = convert2CartData(Topo,p)
+Topo_Cart_orth  = CartData(xyz_grid(-2000:20:2000,-1000:20:1000,0))
+Topo_Cart_orth  = project_CartData(Topo_Cart_orth, Topo, p)
+write_paraview(Topo_Cart,"Topo_Cart_orth");
 
 
 #Topo = import_topo(lon = [-10, 45], lat=[25, 50], file="@earth_relief_20m")
@@ -62,27 +108,61 @@ p=ProjectionPoint(Lon=lon_mean, Lat=lat_mean)
 Topo_Cart = convert2CartData(Topo,p)#Topo_Cart = convert2UTMzone(Topo,p) #convert2UTMzone(Topo,p),convert2CartData(Topo,p)
 write_paraview(Topo_Cart,"Rhein_highest");
 
+#load the csv file for the topography data from the paraview file
+# now we load the csv file for the topography data from the paraview file
+#in vs code you might get asked if you like to install Rainbow CSV
+# ...existing code...
+#df = CSV.read("/Users/Vincent_1/Library/CloudStorage/OneDrive-JGU(2)/HiWi/TrixiBottomTopography_geforkt/TrixiBottomTopography.jl/examples/rh.csv", DataFrame)
+df = CSV.read("/Users/Vincent_1/Library/CloudStorage/OneDrive-JGU(2)/HiWi/TrixiBottomTopography_geforkt/TrixiBottomTopography.jl/examples/modelgenerator3.csv", DataFrame)
+#try to load the data as an orthogonal cartesian grid, vlt geht das aber nicht 
+
 ############################
-#building a .vts file to first get an impression of the topography data in Paraview
+#convert the csv file to a xyz file to be able to use it in TrixiBottomTopography
 
-
+function csv_to_xyz(csv_path, xyz_path)
+    # CSV-Datei einlesen
+    data = CSV.read(csv_path, DataFrame)
     
-#Uses `GMT` to download the topography of a certain region, specified with limits=[lon_min, lon_max, lat_min, lat_max].
-#Sometimes download fails because of the internet connection. We do `maxattempts` to download it.
+    # Spaltenüberschriften bereinigen
+    column_names = names(data)
+    clean_names = [replace(String(name), "Points:" => "") for name in column_names]
+    rename!(data, Dict(zip(column_names, clean_names)))
+    
+    # XYZ-Datei erstellen
+    open(xyz_path, "w") do file
+        for i in 1:size(data, 1)
+            x = data[i, "0"]
+            y = data[i, "1"]
+            z = data[i, "2"]
+            println(file, "$x $y $z")
+        end
+    end
+    
+    println("Konvertierung abgeschlossen. XYZ-Datei wurde erstellt: $xyz_path")
+end
 
-# using GeophysicalModelGenerator, GMT
-# Topo = import_topo([4,20,37,49], file="@earth_relief_01m")
+## Convert the CSV from the topo data to XYZ format:
+csv_to_xyz("/Users/Vincent_1/Library/CloudStorage/OneDrive-JGU(2)/HiWi/TrixiBottomTopography_geforkt/TrixiBottomTopography.jl/examples/modelgenerator3.csv", "/Users/Vincent_1/Library/CloudStorage/OneDrive-JGU(2)/HiWi/TrixiBottomTopography_geforkt/TrixiBottomTopography.jl/examples/modelgenerator3.xyz")
 
-Topo = import_topo(lon = [-10, 45], lat=[25, 50], file="@earth_relief_20m")
+# ############################################################################
+#convert this data to a format which can be used by TrixiBottomTopography, namely a special .txt file format
 
-convert(UTMData, Topo)
+# Define file paths
+data_dir = joinpath(@__DIR__, "data")
+data_dir
+# Download the raw bottom topography data
+path_src_file = joinpath(@__DIR__, "modelgenerator3.xyz")
 
-p=ProjectionPoint(Lon=17.3, Lat=37.5)
+path_out_file_1d_x = joinpath(data_dir, "rhine_data_1d_x_theodor.txt")
+path_out_file_1d_y = joinpath(data_dir, "rhine_data_1d_20_y_theodor.txt")
+path_out_file_2d = joinpath(data_dir, "rhine_data_2d_20_theodor.txt")
 
-convert2UTMzone(Topo,p)
+# Convert data
+convert_dgm_1d(path_src_file, path_out_file_1d_x; excerpt = 20, section = 10)
+convert_dgm_1d(path_src_file, path_out_file_1d_y; excerpt = 20, direction = "y", section = 10)
+convert_dgm_2d(path_src_file, path_out_file_2d; excerpt = 20)
 
-Topo_Cart = convert2CartData(Topo,p)
+# Download one dimensional Rhine bottom data from gist
+#Rhine_data = download("https://gist.githubusercontent.com/maxbertrand1996/19c33682b99bfb1cc3116f31dd49bdb9/raw/d96499a1ffe250bc8e4cca8622779bae61543fd8/Rhine_data_1D_40_x_841.txt")
 
-Topo_Cart_orth  = CartData(xyz_grid(-2000:20:2000,-1000:20:1000,0))
-
-Topo_Cart_orth  = project_CartData(Topo_Cart_orth, Topo, p)
+#
