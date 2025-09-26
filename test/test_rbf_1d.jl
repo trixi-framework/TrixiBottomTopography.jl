210139075bee4ecb79eb1643f06463c2cf082c79
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-09-26 20:57:31 +0200

    shuffle input data in tests

--- test/test_rbf_1d.jl
+++ test/test_rbf_1d.jl
@@ -3,19 +3,24 @@ module TestRBF1D
 using Test
 using TrixiBottomTopography
 using KernelInterpolation: ThinPlateSplineKernel
+using Random
 
 @testset "1D RBF Interpolation" begin
     # Define data path
     root_dir = pkgdir(TrixiBottomTopography)
     data = joinpath(root_dir, "test", "data", "rhine_1d_10_x_1.txt")
-
-    # Define RBF interpolation structure
-    itp = RBFInterpolation1D(data, ThinPlateSplineKernel{1}())
+    x, y = TrixiBottomTopography.parse_txt_1D(data)
+    # Test scattered data by scrambling the data
+    Random.seed!(24601)
+    v = shuffle(collect(1:length(x)))
+    xx = x[v]
+    yy = y[v]
+    itp = RBFInterpolation1D(xx, yy, ThinPlateSplineKernel{1}())
     # Define RBF interpolation function
     itp_func(x) = itp(x)
 
     # Test function at arbitrary point
-    @test itp_func(357555) ≈ 46.19
+    @test isapprox(itp_func(357555), 46.191872246844014; atol = 1e-14)
 end
 
 end # module
