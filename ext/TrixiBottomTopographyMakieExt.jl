3156998a9e08ef7341c3dec698b6462a87fe5f3b
Andrew Winters <andrew.ross.winters@liu.se>
2025-09-26 23:17:43 +0200

    add a deprecated function version

--- ext/TrixiBottomTopographyMakieExt.jl
+++ ext/TrixiBottomTopographyMakieExt.jl
@@ -11,6 +11,11 @@ using TrixiBottomTopography
 import TrixiBottomTopography: evaluate_two_dimensional_interpolant, plot_topography,
                               plot_topography_with_interpolation_knots
 
+# To keep functionality we allow for the old function name `evaluate_bicubicspline_interpolant` to be used.
+# From a front end perspective nothing changes; however, if one starts Julia with `--depwarn=yes`
+# a warning would be thrown when one uses the deprecated function name.
+@deprecate evaluate_bicubicspline_interpolant(spline, x, y) evaluate_two_dimensional_interpolant(spline, x, y)
+
 """
     evaluate_two_dimensional_interpolant(spline, x, y)
 
