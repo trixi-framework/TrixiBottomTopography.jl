ead2a6f691c2958d1254d05589a1b6d82f26c053
Andrew Winters <andrew.ross.winters@liu.se>
2025-12-03 10:28:03 +0100

    Only one PR for GitHub actions (#81)

--- docs/make.jl
+++ docs/make.jl
@@ -127,4 +127,7 @@ makedocs(;
 # for more information.
 deploydocs(repo = "github.com/trixi-framework/TrixiBottomTopography.jl.git",
            devbranch = "main",
-           push_preview = true)
+           # Only push previews if all the relevant environment variables are non-empty.
+           push_preview = all(!isempty,
+                              (get(ENV, "GITHUB_TOKEN", ""),
+                               get(ENV, "DOCUMENTER_KEY", ""))))
